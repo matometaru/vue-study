465fb08927df0e9273327b9235a081429082973d
matometaru <matometaru@gmail.com>
2020-01-28 01:42:21 +0900

    多言語確認

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,35 @@ import vuetify from './plugins/vuetify';
 import firebase from 'firebase';
 import config from '@/config';
 import '@/errorHandler';
+import VueI18n from 'vue-i18n';
 
 Vue.config.productionTip = false;
 
 firebase.initializeApp(config.firebase);
 
+Vue.use(VueI18n);
+const messages = {
+  en: {
+    message: {
+      hello: 'hello world'
+    }
+  },
+  ja: {
+    message: {
+      hello: 'こんにちは、世界'
+    }
+  }
+}
+const i18n = new VueI18n({
+  locale: 'ja',
+  messages,
+})
+
 new Vue({
   router,
   store,
   vuetify,
+  i18n,
   render: (h) => {
     return h(App);
   },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,9 @@
       src="../assets/logo.png"
     >
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <p>{{ $t("message.hello") }}</p>
+    <p>{{ $t("test.hello") }}</p>
+    <p>{{ $t("buttons.save") }}</p>
   </div>
 </template>
 
@@ -12,10 +15,39 @@
 import Vue from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue';
 
+const messages = {
+  en: {
+    test: {
+      hello: 'metal'
+    }
+  },
+  ja: {
+    test: {
+      hello: 'めたる'
+    }
+  }
+}
+const common = {
+  en: {
+    buttons: {
+      save: "Save",
+    }
+  },
+  ja: {
+    buttons: {
+      save: "保存",
+    }
+  }
+}
+
 export default Vue.extend({
   name: 'HomePage',
   components: {
     HelloWorld,
   },
+  i18n: {
+    messages: messages,
+    sharedMessages: common,
+  }
 });
 </script>
